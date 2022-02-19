87ee7174ae64c8ee87c14f51a73691fb6d2ea1e1
RazielEL <amp91@pm.me>
2022-02-19 15:56:03 -0500

    AE3

---
+++ src/javaExeArrays/AE3.java
@@ -0,0 +1,15 @@
+package javaExeArrays;
+
+public class AE3 {
+
+	public static void main(String[] args) {
+		
+		String[] cars = {"Mazda", "Volvo", "Suzuki", "Opel"};
+		
+		for (String i : cars) {
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ src/replit/rep067.java
@@ -0,0 +1,17 @@
+package replit;
+
+public class rep067 {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		for (a=1; a<=5; ++a)
+		{
+		    for (b=1; b<=10; ++b)
+		    {
+		        System.out.print(a*b+"  ");  
+		    }
+		    System.out.println();
+		}    
+	}
+}
